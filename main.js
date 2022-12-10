7a730213211209378193b8380f9fc2cd19c61291
Edward Mata <edward.mata.iii@gmail.com>
2022-12-10 14:32:02 -0800

    so far

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const baseUrl = 'https://pokeapi.co/api/v2/type/'
 
 fetch(baseUrl)
-    .then(function (response ) { return response.json()})
-    // .then(text => {console.log(text.count); })
-    .then {return console.log(response.json())}
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        console.log(randomIndex);
+        const randomPokemon = data.results[randomIndex];
+        console.log(randomPokemon);
+
+
+
+    })
