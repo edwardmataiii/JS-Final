8de0b9ee8c6d7c3496da48b94873156ca708a0be
Edward Mata <edward.mata.iii@gmail.com>
2022-12-11 12:51:08 -0800

    messing with some stuff

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154'                                 
+const baseUrl = 'https://pokeapi.co/api/v2/type/4'                                 
 
 fetch(baseUrl)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        const randomIndex = Math.floor(Math.random() * data.results.length);
-        console.log(randomIndex);
-        const randomPokemon = data.results[randomIndex];
-        console.log(randomPokemon);
+        // const randomIndex = Math.floor(Math.random() * data.results.length);
+        // console.log(randomIndex);
+        // const randomPokemon = data.results[randomIndex];
+        // console.log(randomPokemon);
+        console.log(data, TypePokemon);
 
 
 
     })
+
+
+
+
