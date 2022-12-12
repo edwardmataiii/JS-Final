72292a96ef2bceb0c15a67fa700bf31cd5bed815
Edward Mata <edward.mata.iii@gmail.com>
2022-12-12 15:47:52 -0800

    cleaned up code

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
 const eightBallResponses = [
 
-    'It is certain',
-    'It is decidedly so',
-    'Without a Doubt',
-    'Yes, Definitley',
-    'Ask again later',
-    'Better not tell you now',
-    'Outlook not so good',
-    "don't count on it",
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a Doubt.',
+    'Yes, Definitley.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Outlook not so good.',
+    "don't count on it.",
 
 ];
 
@@ -16,20 +16,15 @@ const eightBallResponses = [
 function askQuestion() {
 const question = prompt("Please Ask Me A Question!", "Question here");
 if (question != null) {
-    document.getElementById("answer").innerHTML = question;    
+    document.getElementById("answer").innerHTML = question;
+    console.log(question)
+    giveReply()
 }};
 
-
-// console.log(eightBallResponses);
-
-// function eightBallAnswer() {
 const randomIndex = Math.floor(Math.random() * eightBallResponses.length);
-console.log(randomIndex)
 const randomResponse = eightBallResponses[randomIndex];
-// }
-
-console.log(randomResponse)
 
 function giveReply() {
     document.getElementById("8ballresponse").innerHTML = randomResponse
-}
+    console.log(randomResponse)
+};
