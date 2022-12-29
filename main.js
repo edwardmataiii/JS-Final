d60b0a0237205945f8f8772c1ba8d82eea588105
Edward Mata <edward.mata.iii@gmail.com>
2022-12-28 19:02:37 -0800

    So far

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154'                                 
+// const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'                                 
 
-fetch(baseUrl)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const randomIndex = Math.floor(Math.random() * data.results.length);
-        console.log(randomIndex);
-        const randomPokemon = data.results[randomIndex];
-        console.log(randomPokemon);
+// fetch(baseUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     });
 
+function pokemonForm() {
+     document.getElementById('pokemonquestion').addEventListener
+    ('submit', (event) => {
+        event.preventDefault();
+    });
 
+    const getpokemonanswer = document.getElementById('pokemonanswer');
+    const pokemon = getpokemonanswer.value; 
+    console.log(pokemon);
 
+    fetch('http://pokeapi.co/api/v2/pokemon/' + pokemon).
+    then((response) => {
+        return response.json();
+    }).then(data => {
+        displayPokemon(data);
     })
+}
+
+function displayPokemon(data) {
+    const h1 = document.createElement('h1');
+    h1.innerText = data.name
+    document.body.appendChild(h1);
+
+}
+
+    
+
+
+    // function getPokemon() {
+    //     const getpokemonanswer = getElementById('pokemonanswer')
+    //     pokemon = getpokemonanswer.value 
+    //     console.log(pokemon)
+
+    // }
