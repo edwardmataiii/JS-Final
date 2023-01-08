efaab0e3c3026f4cd18707a808a9bfb7e48ed786
Edward Mata <edward.mata.iii@gmail.com>
2023-01-08 15:58:39 -0800

    made slight changes

--- main.js
+++ main.js
@@ -8,24 +8,39 @@ function pokemonForm() {
 
     const getpokemonanswer = document.getElementById('pokemonanswer');
     const pokemon = getpokemonanswer.value; 
-    console.log(pokemon);
 
     fetch('http://pokeapi.co/api/v2/pokemon/' + pokemon).
     then((response) => {
         return response.json();
+
     }).then(data => {
         displayPokemon(data);
+        console.log(data)
     })
-}
+};
 
 function displayPokemon(data) {
-    const h1 = document.createElement('h1');
-    h1.innerText = data.name
-    document.body.appendChild(h1);
+    const pokemonName = document.createElement('h1');
+    pokemonName.innerText = data.name
+    document.body.appendChild(pokemonName);
 
-    const img = document.createElement('img');
-    img.src = data.sprites.front_default;
-    document.body.appendChild(img);
+    const pokemonSprites = document.createElement('img');
+    pokemonSprites.src = data.sprites.front_default;
+    document.body.appendChild(pokemonSprites);
+ 
+    const pokemonWeight = document.createElement('h3');
+    pokemonWeight.innerText = "Weight: " + data.weight + " hg";
+    document.body.appendChild(pokemonWeight);
+
+    const pokemonHeight = document.createElement('h3');
+    pokemonHeight.innerText = "Height: " + data.height + " dm";
+    document.body.appendChild(pokemonHeight);
+
+    const pokemonB = document.createElement('h3');
+    pokemonB.innerText = data.types[0, 1];
+    document.body.appendChild(pokemonB);
+    
+    console.log(data.types, "types")
 }
 
     
